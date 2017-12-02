05f426eb58db6e36928757af784450a33799dc92
Kathrin Holzmann <lelith@gmx.de>
2017-12-02 11:08:20 +0100

    part 2

--- d2/day2.js
+++ d2/day2.js
@@ -7,10 +7,27 @@ function calculateSum(){
   for(i = 0; i < numbers.length; i++){
     tmp = numbers[i];
     tmp = tmp.split('\t').map(Number);
-    max = Math.max.apply(null, tmp);
-    min = Math.min.apply(null, tmp);
-    sum =sum+(max-min);
+    sum += findModulo(tmp);
   }
 
   document.querySelector('[data-type="output"]').append(sum);
+}
+
+function findModulo(arr){
+  for (j = 0; j < arr.length; j ++) {
+    for(k = j+1; k < arr.length; k ++){
+      var a = 1;
+      var b = 1;
+      if (arr[j] > arr[k]) {
+        a = arr[j];
+        b = arr[k];
+      } else {
+        a = arr[k];
+        b = arr[j];
+      }
+      if (a%b==0) {
+        return a/b;
+      }
+    }
+  }
 }
