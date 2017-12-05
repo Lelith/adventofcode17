6918ef144718e856613f0bab93b042f9a8970347
Kathrin Holzmann <lelith@gmx.de>
2017-12-05 14:05:12 +0100

    day 5 part 1

---
+++ d5/day5.js
@@ -0,0 +1,16 @@
+function calculateSum(){
+  var input = document.querySelector('[data-type="input"]').innerHTML;
+  input = input.split('\n').map(Number);
+  var pointer = 0;
+  var sum = 0;
+  console.log(input);
+  while ((pointer < input.length) && (pointer >-1)) {
+    offset = input[pointer];
+    newPos = pointer + offset;
+    input[pointer] ++;
+    pointer = newPos;
+    sum ++;
+  }
+
+  document.querySelector('[data-type="output"]').append(sum);
+}
